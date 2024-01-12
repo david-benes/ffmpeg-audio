dfcc36cbcd901d92d2cfdf05ebd592cd6ad221f6
David Beneš <dbenes@gmail.com>
2024-01-12 14:32:36 +0100

    replaces custom build with modified, community supported ffmpeg-static

---
+++ ffmpeg-static/build-targets.sh
@@ -0,0 +1,178 @@
+#!/bin/sh
+
+set -e
+set -u
+
+jflag=
+jval=2
+rebuild=0
+download_only=0
+uname -mpi | grep -qE 'x86|i386|i686' && is_x86=1 || is_x86=0
+
+while getopts 'j:Bd' OPTION
+do
+  case $OPTION in
+  j)
+      jflag=1
+      jval="$OPTARG"
+      ;;
+  B)
+      rebuild=1
+      ;;
+  d)
+      download_only=1
+      ;;
+  ?)
+      printf "Usage: %s: [-j concurrency_level] (hint: your cores + 20%%) [-B] [-d]\n" $(basename $0) >&2
+      exit 2
+      ;;
+  esac
+done
+shift $(($OPTIND - 1))
+
+if [ "$jflag" ]
+then
+  if [ "$jval" ]
+  then
+    printf "Option -j specified (%d)\n" $jval
+  fi
+fi
+
+[ "$rebuild" -eq 1 ] && echo "Reconfiguring existing packages..."
+[ $is_x86 -ne 1 ] && echo "Not using yasm or nasm on non-x86 platform..."
+
+cd `dirname $0`
+ENV_ROOT=`pwd`
+. ./env.source
+
+# check operating system
+OS=`uname`
+platform="unknown"
+
+case $OS in
+  'Darwin')
+    platform='darwin'
+    ;;
+  'Linux')
+    platform='linux'
+    ;;
+esac
+
+#if you want a rebuild
+#rm -rf "$BUILD_DIR" "$TARGET_DIR"
+mkdir -p "$BUILD_DIR" "$TARGET_DIR" "$DOWNLOAD_DIR" "$BIN_DIR"
+
+#download and extract package
+download(){
+  filename="$1"
+  if [ ! -z "$2" ];then
+    filename="$2"
+  fi
+  ../download.pl "$DOWNLOAD_DIR" "$1" "$filename" "$3" "$4"
+  #disable uncompress
+  REPLACE="$rebuild" CACHE_DIR="$DOWNLOAD_DIR" ../fetchurl "http://cache/$filename"
+}
+
+echo "#### FFmpeg static build ####"
+
+#this is our working directory
+cd $BUILD_DIR
+
+download \
+  "v0.1.6.tar.gz" \
+  "fdk-aac.tar.gz" \
+  "223d5f579d29fb0d019a775da4e0e061" \
+  "https://github.com/mstorsjo/fdk-aac/archive"
+
+download \
+  "lame-3.99.5.tar.gz" \
+  "" \
+  "84835b313d4a8b68f5349816d33e07ce" \
+  "http://downloads.sourceforge.net/project/lame/lame/3.99"
+
+download \
+  "n4.0.tar.gz" \
+  "ffmpeg4.0.tar.gz" \
+  "4749a5e56f31e7ccebd3f9924972220f" \
+  "https://github.com/FFmpeg/FFmpeg/archive"
+
+[ $download_only -eq 1 ] && exit 0
+
+TARGET_DIR_SED=$(echo $TARGET_DIR | awk '{gsub(/\//, "\\/"); print}')
+
+
+echo "*** Building fdk-aac ***"
+cd $BUILD_DIR/fdk-aac*
+[ $rebuild -eq 1 -a -f Makefile ] && make distclean || true
+autoreconf -fiv
+[ ! -f config.status ] && ./configure --prefix=$TARGET_DIR --disable-shared
+make -j $jval
+make install
+
+echo "*** Building mp3lame ***"
+cd $BUILD_DIR/lame*
+# The lame build script does not recognize aarch64, so need to set it manually
+uname -a | grep -q 'arm64' && lame_build_target="--build=arm-linux" || lame_build_target=''
+[ $rebuild -eq 1 -a -f Makefile ] && make distclean || true
+[ ! -f config.status ] && ./configure --prefix=$TARGET_DIR --enable-nasm --disable-shared $lame_build_target
+make
+make install
+
+
+# FFMpeg
+echo "*** Building FFmpeg ***"
+cd $BUILD_DIR/FFmpeg*
+[ $rebuild -eq 1 -a -f Makefile ] && make distclean || true
+
+if [ "$platform" = "linux" ]; then
+  [ ! -f config.status ] && PATH="$BIN_DIR:$PATH" \
+  PKG_CONFIG_PATH="$TARGET_DIR/lib/pkgconfig" ./configure \
+    --prefix="$TARGET_DIR" \
+    --pkg-config-flags="--static" \
+    --extra-cflags="-I$TARGET_DIR/include" \
+    --extra-ldflags="-L$TARGET_DIR/lib" \
+    --extra-libs="-lpthread -lm -lz" \
+    --extra-ldexeflags="-static" \
+    --bindir="$BIN_DIR" \
+    --disable-everything \
+    --enable-demuxer=aac,flac,mp3,mp4,mov,wav \
+    --enable-muxer=aac,flac,mp3,mp4,mov,wav,segment \
+    --enable-decoder=aac,alac,flac,mp3,pcm* \
+    --enable-parser=aac,mpegaudio,flac \
+    --enable-encoder=libmp3lame \
+    --enable-libmp3lame \
+    --enable-protocol=file \
+    --enable-bsf=aac_adtstoasc \
+    --enable-libfdk-aac \
+    --enable-nonfree \
+    --enable-filter=aresample,silencedetect
+
+
+elif [ "$platform" = "darwin" ]; then
+  [ ! -f config.status ] && PATH="$BIN_DIR:$PATH" \
+  PKG_CONFIG_PATH="${TARGET_DIR}/lib/pkgconfig:/usr/local/lib/pkgconfig:/usr/local/share/pkgconfig:/usr/local/Cellar/openssl/1.0.2o_1/lib/pkgconfig" ./configure \
+    --cc=/usr/bin/clang \
+    --prefix="$TARGET_DIR" \
+    --pkg-config-flags="--static" \
+    --extra-cflags="-I$TARGET_DIR/include" \
+    --extra-ldflags="-L$TARGET_DIR/lib" \
+    --extra-ldexeflags="-Bstatic" \
+    --bindir="$BIN_DIR" \
+    --disable-everything \
+    --enable-demuxer=aac,flac,mp3,mp4,mov,wav \
+    --enable-muxer=aac,flac,mp3,mp4,mov,wav,segment \
+    --enable-decoder=aac,alac,flac,mp3,pcm* \
+    --enable-parser=aac,mpegaudio,flac \
+    --enable-encoder=libmp3lame \
+    --enable-libmp3lame \
+    --enable-protocol=file \
+    --enable-bsf=aac_adtstoasc \
+    --enable-libfdk-aac \
+    --enable-nonfree \
+    --enable-filter=aresample,silencedetect
+fi
+
+PATH="$BIN_DIR:$PATH" make -j $jval
+make install
+make distclean
+hash -r
